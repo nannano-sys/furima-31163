56f67806d5380fa062ddaeddfe910cf86b23ae57
nannano-sys <nannano1868@gmeil.com>
2020-11-05 22:46:51 +0900

    factorybotを追記した

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  FactoryBot.define do
+    factory :user do
+           
+    end
+  end
 
   with_options presence: true do
     validates :nickname
