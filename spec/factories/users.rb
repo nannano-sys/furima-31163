23cd373fdf1794f41cc4d60daf3a9c84f52f87e5
nannano-sys <nannano1868@gmeil.com>
2020-11-06 19:38:55 +0900

    factorybotにインスタンスを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :user do
-    
+      transient do
+        person { Gimei.name } #人名に漢字とカタカナを適用するための記述
+      end
+    nickname {Faker::Name.initials}
+    email    {Faker::Internet.free_email}
+    password {"a1"+Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name {person.first.kanji}
+    family_name {person.last.kanji}
+    first_name_kana {person.last.katakana}
+    family_name_kana {person.last.katakana}
+    birthday {Faker::Date.backward}
   end
 end
