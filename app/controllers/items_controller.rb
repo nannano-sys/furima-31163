6ffb1221c4a8c35408fd35865e8d4aa3a20d20bd
nannano-sys <nannano1868@gmeil.com>
2020-11-09 20:55:34 +0900

    itemsコントローラを編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,5 +10,17 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def items_params
+    params.require(:item).permit(:mane, :explanation, :category_id, :status_id, :burden_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
   end
 end
