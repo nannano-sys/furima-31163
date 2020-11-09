e6fbad90ed13a3825ace46a42a5bb3599b7b30ba
nannano-sys <nannano1868@gmeil.com>
2020-11-09 17:12:46 +0900

    バリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :area_id
+    validates :days_id
+    validates :price, numericality: {greater_than_or_erual_to: 300 , less_than_or_equal_to: 9999999}, format: {with:/\A[a-zA-Z0-9]+\z/, message: "Half-width number"}
+    validates :user
+  end
+
+  belongs_to       :user
+  has_one          :order
+  has_one_attached :image
 end
