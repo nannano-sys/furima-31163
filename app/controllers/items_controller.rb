1767f49f425d23c54a4f0187f737e19fc5100afc
nannano-sys <nannano1868@gmeil.com>
2020-11-09 17:12:25 +0900

    コントローラにnewとcreateのメソッドを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+
   def index
   end
 
-  def new 
+  def new
+    @item = Item.new
+  end
+
+  def create
   end
 end
