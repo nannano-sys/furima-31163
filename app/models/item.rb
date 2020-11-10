b2451ce0bd39e3c98743c8916ce921b0e02fc30e
nannano-sys <nannano1868@gmeil.com>
2020-11-10 13:04:10 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,14 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :explanation
-    validates :category_id, numericality: { other_than: 1 }  #ジャンルの選択が---のときは選択できないようにする
-    validates :status_id, numericality: { other_than: 1 }  
-    validates :burden_id, numericality: { other_than: 1 } 
-    validates :area_id, numericality: { other_than: 1 } 
-    validates :days_id, numericality: { other_than: 1 } 
-    validates :price, numericality: {greater_than_or_erual_to: 300 , less_than_or_equal_to: 9999999}, format: {with:/\A[a-zA-Z0-9]+\z/, message: "Half-width number"}
+    validates :category_id, numericality: { other_than: 1, message: 'Select'}#ジャンルの選択が---のときは選択できないようにする
+    validates :status_id, numericality: { other_than: 1, message: 'Select' }  
+    validates :burden_id, numericality: { other_than: 1, message: 'Select'} 
+    validates :area_id, numericality: { other_than: 1, message: 'Select'}
+    validates :days_id, numericality: { other_than: 1, message: 'Selectk' } 
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range'}, format: {with:/\A[a-zA-Z0-9]+\z/, message: "Half-width number"}
     validates :user
+    validates :image
   end
 
   belongs_to       :user
