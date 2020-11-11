946dfced4e390cbe86b1733014d6f4aba95fa42e
nannano-sys <nannano1868@gmeil.com>
2020-11-11 15:26:52 +0900

    テストコードを修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -35,33 +35,63 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category Select")
       end
+      it "カテゴリの情報が存在しなければ出品できない(空の場合)" do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
       it "商品の状態の情報が存在しなければ出品できない" do
         @item.status_id = "1"
         @item.valid?
         expect(@item.errors.full_messages).to include("Status Select")
+      end
+      it "商品の状態の情報が存在しなければ出品できない(空の場合)" do
+        @item.status_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
       end
       it "配送料の負担の情報が存在しなければ出品できない" do
         @item.burden_id = "1"
         @item.valid?
         expect(@item.errors.full_messages).to include("Burden Select")
       end
+      it "配送料の負担の情報が存在しなければ出品できない(空の場合)" do
+        @item.burden_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Burden can't be blank")
+      end
       it "配送元の地域の情報が存在しなければ出品できない" do
         @item.area_id = "1"
         @item.valid?
         expect(@item.errors.full_messages).to include("Area Select")
+      end
+      it "配送元の地域の情報が存在しなければ出品できない(空の場合)" do
+        @item.area_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank")
       end
       it "発送までの日数の情報が存在しなければ出品できない" do
         @item.days_id = "1"
         @item.valid?
         expect(@item.errors.full_messages).to include("Days Select")
       end
+      it "発送までの日数の情報が存在しなければ出品できない(空の場合)" do
+        @item.days_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days can't be blank")
+      end
       it "価格の情報が存在しなければ出品できない" do
         @item.price = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it "価格の範囲が¥300~¥9,999,999の間でなければ出品できない" do
+      it "価格の範囲が¥300~¥9,999,999の間でなければ出品できない(¥300以下の場合)" do
         @item.price = "100"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Out of setting range")
+      end
+      it "価格の範囲が¥300~¥9,999,999の間でなければ出品できない(¥9,999,999以上の場合)" do
+        @item.price = "10000000"
         @item.valid?
         expect(@item.errors.full_messages).to include("Price Out of setting range")
       end
