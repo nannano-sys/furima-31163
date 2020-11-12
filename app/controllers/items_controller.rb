f533fdab51621a33b59ac48521c5cb22f2b2c0b1
nannano-sys <nannano1868@gmeil.com>
2020-11-12 15:58:57 +0900

    rubocop実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
@@ -23,8 +22,8 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-
   private
+
   def items_params
     params.require(:item).permit(:name, :explanation, :category_id, :status_id, :burden_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
