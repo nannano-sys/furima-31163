0923724f5372a6e95d200b331e0440c996b05683
nannano-sys <nannano1868@gmeil.com>
2020-11-12 20:02:08 +0900

    コントローラ編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,13 +22,23 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
+  def edit  
+      @item = Item.find(params[:id])
+      unless current_user == @item.user
+        redirect_to new_user_session_path
+      end
   end
 
   def update
+    @item = Item.find(params[:id])
+      if @item.update(items_params)
+        redirect_to root_path
+      else
+        render :edit
+      end
   end
 
-  
+
   private
 
   def items_params
