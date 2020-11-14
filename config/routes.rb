1334770c612e26aba98ede757292a53b808de050
nannano-sys <nannano1868@gmeil.com>
2020-11-14 18:12:09 +0900

    ルーティングをネスト

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
 
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :orders, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :orders, only: [:index, :new, :create]
+  end
 end
