3966e343b75bf42aaff33eaa46e2cf0f8c60db17
nannano-sys <nannano1868@gmeil.com>
2020-11-14 18:36:57 +0900

    orderモデルを編集

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,7 @@ class Order < ApplicationRecord
   belongs_to :item
   has_one :address
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
