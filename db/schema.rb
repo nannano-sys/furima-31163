0b668e792c0f9b3c5aadfed0ad1878c89f75b174
nannano-sys <nannano1868@gmeil.com>
2020-11-14 18:37:30 +0900

    Prefectureモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_11_13_092122) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_cord", null: false
-    t.integer "prefectures_id", null: false
+    t.integer "prefecture_id", null: false
     t.string "municipality", null: false
     t.string "house_number", null: false
     t.string "building_name"
