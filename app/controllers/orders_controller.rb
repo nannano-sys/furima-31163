d54c08c96c8646471baf011fa45f0483fcc322fc
nannano-sys <nannano1868@gmeil.com>
2020-11-17 21:15:15 +0900

    環境変数設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,16 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    redirect_to root_path if @item.order
     @item_order = ItemOrder.new
   end
-
-  # def new
-  #   @item_order = ItemOrder.new
-  # end
+  
 
   def create
      @item = Item.find(params[:item_id]) 
      @item_order = ItemOrder.new(order_params)
-    if @item_order.valid?
+     if @item_order.valid?
+      pay_item
       @item_order.save
       redirect_to root_path
     else
@@ -22,7 +21,15 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:item_order).permit(:postal_cord, :prefecture_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:item_order).permit(:postal_cord, :prefecture_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token] )
   end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,  # 商品の情報
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
+  end
 end
