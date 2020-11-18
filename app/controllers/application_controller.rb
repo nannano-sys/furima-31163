77810ee77860418c76ab80d0d66d8fb314c44d19
nannano-sys <nannano1868@gmeil.com>
2020-11-18 15:19:08 +0900

    basic_authメソッド定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller? #devise用のストロングパラメータ
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birthday])
-    #:sign_upの記述で、サインインの時にnicknameを保存するのを許可している
+    #:sign_upの記述で、サインインの時にnickname以下を保存するのを許可している
+  end
+
+  def basic_auth #basic認証のメソッド
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
   end
 end
